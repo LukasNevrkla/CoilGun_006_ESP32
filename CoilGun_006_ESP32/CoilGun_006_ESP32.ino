54edae4de32a98b0c5cb009661d8ff69837aab02
LukasNevrkla <42738049+LukasNevrkla@users.noreply.github.com>
2019-01-29 15:07:47 +0100

    Přidejte soubory projektu.

---
+++ CoilGun_006_ESP32/CoilGun_006_ESP32.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		CoilGun_006_ESP32.ino
+ Created:	29.1.2019 15:07:38
+ Author:	luky
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
